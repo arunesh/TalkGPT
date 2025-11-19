ca21d52cfac89a7afd53756e5a6c608090b0e58e
Arunesh Mishra <aruneshm@gmail.com>
2025-11-18 16:31:08 -0800

    Initial Commit

---
+++ TalkGPT/TalkGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkGPTApp.swift
+//  TalkGPT
+//
+//  Created by Arun Mishra on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TalkGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
